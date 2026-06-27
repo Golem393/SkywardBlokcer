41d577bccb06093fbe4be28a25893a29e25cb491
jacob2011402 <jlim2011402@gmail.com>
2026-06-27 16:14:25 +0900

    updated the version code & name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.skywardblocker"
         minSdk = 24
         targetSdk = 36
-        versionCode = 116
-        versionName = "1.117"
+        versionCode = 119
+        versionName = "1.119"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
